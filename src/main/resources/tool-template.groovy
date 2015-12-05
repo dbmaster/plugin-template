15a6d872e3c5e58286c996f14afeb651759c2c10
Slava Christin <slava@branegy.com>
2015-12-05 12:26:44 -0500

    Add plugin/tool files

---
+++ src/main/resources/tool-template.groovy
@@ -0,0 +1,1 @@
+println p_sample
---
+++ src/test/groovy/PluginTemplateIT.groovy
@@ -0,0 +1,16 @@
+import io.dbmaster.testng.BaseToolTestNGCase
+
+import static org.testng.Assert.assertTrue
+import org.testng.annotations.Test
+
+
+public class PluginTemplateIT extends BaseToolTestNGCase {
+
+    @Test
+    public void test() {
+        // def parameters = [ "p_contact_name"  :  "test" ]
+        // String result = tools.toolExecutor("dbmaster-contact-links", parameters).execute()
+        // assertTrue(result.contains("Contact"), "Unexpected search results ${result}");
+        // assertTrue(result.contains("Related to"), "Unexpected search results ${result}");
+    }
+}
